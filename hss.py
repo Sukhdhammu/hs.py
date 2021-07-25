cd90303dc57a1b6a6ccc5f6b41eb5331436e7c82
sukhdhammu <preetkaur7933@gmail.com>
2021-07-25 09:57:55 +0530

    Initial commit

---
+++ hss.py
@@ -0,0 +1,8 @@
+n1 = float(input("Enter first number"))
+n2 = float(input("Enter second number"))
+op = input("enter operation")
+if op == "+":
+    print(n1+n2)
+elif op == "-":
+    print(n1-n2)
+    
